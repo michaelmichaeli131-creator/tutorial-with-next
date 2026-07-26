e5d1ce8e8a7bcbfb8e14a63a3e2d41bd4e52c586
michaelmichaeli131-creator <michael.michaeli131@gmail.com>
2026-07-26 15:50:53 +0300

    Cache modular styles for offline PWA

--- blast-rush-arena/client/sw.js
+++ blast-rush-arena/client/sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'blast-rush-arena-v5';
-const ASSETS = ['/', '/index.html', '/styles.css', '/net.js', '/game.js', '/manifest.webmanifest', '/icons/icon-192.svg', '/icons/icon-512.svg'];
+const ASSETS = ['/', '/index.html', '/styles.css', '/styles/01.css', '/styles/02.css', '/styles/03.css', '/styles/04.css', '/styles/05.css', '/net.js', '/game.js', '/manifest.webmanifest', '/icons/icon-192.svg', '/icons/icon-512.svg'];
 self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS))));
 self.addEventListener('activate', (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))));
 self.addEventListener('fetch', (event) => {
